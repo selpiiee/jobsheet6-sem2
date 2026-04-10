afbd50e5c3b57809e3acd6a496c8ed792b1c8527
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-04-10 14:57:37 +0700

    Modify Selection & Insertion - 2

--- TopStudent21.java
+++ TopStudent21.java
@@ -1,4 +1,6 @@
+
 public class TopStudent21 {
+
     Student21[] listStudents;
     int idx;
 
@@ -25,12 +27,40 @@ public class TopStudent21 {
     public void bubbleSort() {
         for (int i = 0; i < idx - 1; i++) {
             for (int j = 0; j < idx - i - 1; j++) {
-                if (listStudents[j].gpa < listStudents[j +1].gpa) {
+                if (listStudents[j].gpa < listStudents[j + 1].gpa) {
                     Student21 temp = listStudents[j];
                     listStudents[j] = listStudents[j + 1];
                     listStudents[j + 1] = temp;
                 }
             }
+        }
+    }
+
+    public void selectionSort() {
+        for (int i = 0; i < idx; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < idx; j++) {
+                if (listStudents[j].gpa < listStudents[minIndex].gpa) {
+                    minIndex = j;
+                }
+            }
+            Student21 temp = listStudents[minIndex];
+            listStudents[minIndex] = listStudents[i];
+            listStudents[i] = temp;
+        }
+    }
+
+    public void insertionSort() {
+        for (int i = 1; i < idx; i++) {
+            Student21[] listStudents1 = listStudents;
+            Student21 temp = listStudents1[i];
+            int j = i;
+
+            while (j > 0 && listStudents1[j - 1].gpa > temp.gpa) {
+                listStudents1[j] = listStudents1[j - 1];
+                j--;
+            }
+            listStudents1[j] = temp;
         }
     }
 }
