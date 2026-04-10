15e61b00cc2dba75f4475d556dacae9029a77696
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-04-10 14:57:22 +0700

    Modify Selection & Insertion - 1

--- StudentDemo21.java
+++ StudentDemo21.java
@@ -1,19 +1,51 @@
+import java.util.Scanner;
+
 public class StudentDemo21 {
     public static void main(String[] args) {
-        TopStudent21 topStudents = new TopStudent21(5);
+        Scanner sc = new Scanner(System.in);
 
-        topStudents.add(new Student21("2201", "Alice", "A", 3.9));
-        topStudents.add(new Student21("2202", "Bob", "B", 3.7));
-        topStudents.add(new Student21("2203", "Charlie", "C", 3.8));
-        topStudents.add(new Student21("2204", "David", "D", 3.6));
-        topStudents.add(new Student21("2205", "Eve", "E", 4.0));
+        System.out.print("Enter the number of student: ");
+        int size = sc.nextInt();
+        sc.nextLine();
+
+        TopStudent21 topStudents = new TopStudent21(size);
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Student data to-" + (i + 1));
+            System.out.print("NIM\t: ");
+            String nim = sc.nextLine();
+            System.out.print("Name\t: ");
+            String name = sc.nextLine();
+            System.out.print("Class\t: ");
+            String studentClass = sc.nextLine();
+            System.out.print("GPA\t: ");
+            double gpa = sc.nextDouble();
+            sc.nextLine();
+
+            topStudents.add(new Student21(nim, name, studentClass, gpa));
+            System.out.println();
+        }
 
         System.out.println("Original student list: ");
         topStudents.print();
-        System.out.println(" ");
+        System.out.println();
 
         topStudents.bubbleSort();
+
         System.out.println("Sorted student list (by GPA, descending): ");
         topStudents.print();
+        System.out.println();
+
+        topStudents.selectionSort();
+        System.out.println("Sorted student list (by GPA, ascending) using Selection sort: ");
+        topStudents.print();
+        System.out.println();
+
+        topStudents.insertionSort();
+        System.out.println("Sorted list (by GPA, ascending) using Insertion sort: ");
+        topStudents.print();
+
+        sc.close();
     }
+
 }
